56afd64c817edb5609aeb91c09a3a6b825d66e25
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-07-23 18:49:07 -0300

    Realizado e js

---
+++ brakingRecords/BreakingRecords.java
@@ -0,0 +1,10 @@
+package brakingRecords;
+
+public class BreakingRecords{
+   
+    public static void breakingRecords (String[] args){}
+    
+    public static void main(String[] args){
+        System.out.println("Soy un ejecutable");
+    } 
+}

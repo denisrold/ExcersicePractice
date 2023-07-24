d75c1adf33cc6b2c45c4766b5f401629d192d692
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-07-24 19:25:17 -0300

    Resolved in Java

--- mediasElement/MediasElement.java
+++ mediasElement/MediasElement.java
@@ -1,14 +1,41 @@
 package mediasElement;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MediasElement {
-    public static Integer main(String[] args){
-        mediaElement();
-    }
     public static Integer mediaElement(List<Integer> arr){
-        System.out.println(arr);
+        List<Integer> arrayNumbers = new ArrayList<>();
+        arrayNumbers = arr;
+        Boolean flag = true;
+         while (flag) {
+            flag=false;
+            for (int i = 0; i < arrayNumbers.size() - 1; i++) {
+            if (arrayNumbers.get(i) > arrayNumbers.get(i + 1)) {
+            Integer aux = arrayNumbers.get(i);
+            arrayNumbers.set(i, arrayNumbers.get(i + 1));
+            arrayNumbers.set(i + 1,aux);
+            flag = true;
+         }
+        }
+        }
+        int result = (int) Math.floor(arrayNumbers.size() / 2.0);
+        System.out.println(arrayNumbers.get(result));
         return 1;
     }
-
+     
+    public static void  main(String[] args){
+      List<Integer> arrayNumber = new ArrayList<>();
+      arrayNumber.add(0);
+      arrayNumber.add(1);
+      arrayNumber.add(2);
+      arrayNumber.add(4);
+      arrayNumber.add(6);
+      arrayNumber.add(5);
+      arrayNumber.add(3);
+      
+        mediaElement(arrayNumber);
+    }
 }
+
+

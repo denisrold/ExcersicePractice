a1bef469fc8c56de869ce44e7592299449af70fc
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-07-24 13:11:26 -0300

    Resolved JAva

--- camelCase4/camelCase.js
+++ camelCase4/camelCase.js
@@ -1,49 +1,45 @@
 const camelCase = (input) => {
-  const arrayOutput = [];
-  for (i = 0; i < input.length; i++) {
-    let inputSplit = input[i].split(";");
-    let inputMethod;
-    let auxMethod;
-    let finalCase;
+  let inputSplit = input.split(";");
+  let inputMethod;
+  let auxMethod;
+  let finalCase;
 
-    if (inputSplit[0] == "S") {
-      inputMethod = inputSplit[inputSplit.length - 1].split(/(?=[A-Z])/);
-      for (let i = 0; i < inputMethod.length; i++) {
-        inputMethod[i] = inputMethod[i].toLowerCase();
-      }
-      if (inputSplit[1] == "M") {
-        auxMethod = inputMethod[inputMethod.length - 1].split("(").shift();
-        inputMethod.pop();
-        inputMethod.push(auxMethod);
-      }
-      inputMethod = inputMethod.join(" ");
-      finalCase = inputMethod;
+  if (inputSplit[0] == "S") {
+    inputMethod = inputSplit[inputSplit.length - 1].split(/(?=[A-Z])/);
+    for (let i = 0; i < inputMethod.length; i++) {
+      inputMethod[i] = inputMethod[i].toLowerCase();
     }
-
-    if (inputSplit[0] == "C") {
-      inputMethod = inputSplit.slice(2);
-
-      inputMethod = inputMethod.join().split(" ");
-      auxMethod = [];
-      for (let i = 0; i < inputMethod.length; i++) {
-        if (inputSplit[1] != "C" && i == 0) {
-          auxMethod.push(inputMethod[i]);
-          continue;
-        }
-        const firstLetter = inputMethod[i].charAt(0).toUpperCase();
-        const restWord = inputMethod[i].slice(1);
-        let WordUpper = firstLetter + restWord;
-
-        auxMethod.push(WordUpper);
-      }
-      if (inputSplit[1] == "M") auxMethod.push("()");
-      inputMethod = auxMethod.join("");
-      finalCase = inputMethod;
+    if (inputSplit[1] == "M") {
+      auxMethod = inputMethod[inputMethod.length - 1].split("(").shift();
+      inputMethod.pop();
+      inputMethod.push(auxMethod);
     }
-    arrayOutput.push(finalCase);
+    inputMethod = inputMethod.join(" ");
+    finalCase = inputMethod;
   }
-  console.log(arrayOutput);
+
+  if (inputSplit[0] == "C") {
+    inputMethod = inputSplit.slice(2);
+
+    inputMethod = inputMethod.join().split(" ");
+    auxMethod = [];
+    for (let i = 0; i < inputMethod.length; i++) {
+      if (inputSplit[1] != "C" && i == 0) {
+        auxMethod.push(inputMethod[i]);
+        continue;
+      }
+      const firstLetter = inputMethod[i].charAt(0).toUpperCase();
+      const restWord = inputMethod[i].slice(1);
+      let WordUpper = firstLetter + restWord;
+
+      auxMethod.push(WordUpper);
+    }
+    if (inputSplit[1] == "M") auxMethod.push("()");
+    inputMethod = auxMethod.join("");
+    finalCase = inputMethod;
+  }
+  console.log(finalCase);
 };
 
-const input = ["C;M;white sheet of paper", "C;M;white sheet of paper"];
+const input = "S;M;plasticCup()";
 console.log(camelCase(input));

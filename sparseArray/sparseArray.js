e3b4ba91c56b9815ee80b70e558cd53689f4d75a
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-07-24 17:00:27 -0300

    Resuelto con diccionario

--- sparseArray/sparseArray.js
+++ sparseArray/sparseArray.js
@@ -1,4 +1,20 @@
-const sparseArray = (string, query) => {
+const sparseArray = (strings, queries) => {
+  const frecuencyMap = {};
+
+  for (const string of strings) {
+    frecuencyMap[string] = (frecuencyMap[string] || 0) + 1;
+    console.log(frecuencyMap);
+  }
+  const result = [];
+  for (const query of queries) {
+    const frecuency = frecuencyMap[query] || 0;
+    console.log(frecuency);
+    result.push(frecuency);
+  }
+
+  return result;
+};
+/*const sparseArray = (string, query) => {
   let total = [];
   let sum = 0;
   for (let i = 0; i < query.length; i++) {
@@ -11,7 +27,7 @@ const sparseArray = (string, query) => {
     sum = 0;
   }
   return total;
-};
+};*/
 
 const q = ["aba", "xzxb", "ab"];
 const s = ["aba", "baba", "aba", "xzxb"];

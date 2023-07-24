88bf461627f93168a8149a674cd4104e8ff95a22
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-07-24 15:59:32 -0300

    Resolved in typeScrit

---
+++ divisibleSumPairs/divisibleSumPairs.ts
@@ -0,0 +1,17 @@
+function divisibleSumPairs2(n:number , k:number, ar:number[]):number {
+    let total: number= 0;
+  
+    for (let i = 0; i < ar.length; i++) {
+      for (let j = i + 1; j < ar.length; j++) {
+        if ((ar[i] + ar[j]) % k == 0) {
+          total++;
+        }
+      }
+    }
+  
+    return total;
+  }
+  
+  let arr2:number[] = [1, 3, 2, 6, 1, 2];
+  console.log(divisibleSumPairs(6, 3, arr));
+  

f4f5f9318324aa8f3de3b6610f786e26a4275b8c
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-07-27 19:17:48 -0300

    Realzado en java

--- diagonalDiference/DiagonalDiference.java
+++ diagonalDiference/DiagonalDiference.java
@@ -1,5 +1,30 @@
 package diagonalDiference;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DiagonalDiference {
-    
+    public static void main (String[] args){
+        List<List<Integer>> matrix = new ArrayList<>();
+        matrix.add(List.of(11, 2, 4));
+        matrix.add(List.of(4, 5, 6));
+        matrix.add(List.of(10, 8, -12));
+        
+        diagonalDifference(matrix);
+    }
+
+   public static Integer diagonalDifference(List<List<Integer>> arr) {
+    // Write your code here
+        int n = arr.size();
+    int leftDiagonalSum = 0;
+    int rightDiagonalSum = 0;
+
+        for(Integer i =0;i <n;i++){
+            leftDiagonalSum += arr.get(i).get(i);
+            rightDiagonalSum += arr.get(i).get((n - 1) - i);
+        }
+    int difference = Math.abs(leftDiagonalSum - rightDiagonalSum);
+    System.out.println(difference);
+        return difference;
+    }
 }

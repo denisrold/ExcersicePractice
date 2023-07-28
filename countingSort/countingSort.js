3329e77623c2bc2c5689fd8e50eaa8c70f92d149
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-07-27 22:24:50 -0300

    Resuelto en js

---
+++ countingSort/countingSort.js
@@ -0,0 +1,21 @@
+const countinSort = (arrNumber) => {
+  console.log(arrNumber);
+  let CountingMap = [];
+
+  for (let i = 0; i < arrNumber.length; i++) {
+    if (!CountingMap[i]) CountingMap[i] = 0;
+    CountingMap[arrNumber[i]] = CountingMap[arrNumber[i]] + 1;
+  }
+
+  return CountingMap;
+};
+const arrNumber = [
+  12, 55, 39, 97, 72, 27, 21, 4, 82, 95, 3, 6, 43, 45, 35, 29, 69, 88, 58, 32,
+  61, 20, 24, 100, 45, 71, 90, 16, 67, 56, 13, 86, 19, 54, 23, 46, 42, 47, 0,
+  97, 49, 22, 33, 15, 34, 12, 23, 14, 2, 90, 1, 53, 41, 17, 75, 39, 62, 27, 84,
+  46, 2, 64, 89, 19, 45, 26, 72, 43, 73, 8, 62, 20, 74, 55, 62, 10, 83, 29, 99,
+  69, 97, 93, 46, 56, 81, 23, 53, 53, 93, 84, 49, 93, 55, 93, 56, 58, 45, 38,
+  91, 35, 25, 72, 90, 94,
+];
+
+console.log(countinSort(arrNumber));

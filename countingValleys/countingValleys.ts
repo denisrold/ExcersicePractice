b8d4c6f75b0f3d7757076cad2d06b1fc8d3487fd
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-07-29 19:06:57 -0300

    Resuelto en ts

--- countingValleys/countingValleys.ts
+++ countingValleys/countingValleys.ts
@@ -1,7 +1,17 @@
-const countingValley2 = (valley:string[]):number => {
-    console.log(valley);
-    return 0;
+const countingValley2 = (arr:string[]):number => {
+    let level:number = 0;
+    let valleys:number = 0;
+    let isInValley = false;
+ 
+for(let i =0; i < arr.length; i++){
+    let step2:string = arr[i];
+    if(step2 == "U"){level ++}
+    else if(step2 == "D"){level --}
+    if(level < 0 && !isInValley){isInValley = true}
+    if(level >= 0 && isInValley){isInValley = false; valleys++}
+ }
+ console.log(valleys)
+ return valleys;
 };
-  
-  const valleys2:string[] = ["/","/","/"];
-  console.log(countingValley2(valleys2));
+const arr2:string[] = ["U", "D", "D", "D", "U", "D", "U", "U"];
+  console.log(countingValley2(arr2));

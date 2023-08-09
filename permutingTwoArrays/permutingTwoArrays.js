7a5d9a46193697b352c4265cfc0f61c309bd61b9
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-08-08 22:05:13 -0300

    Resuelto

--- permutingTwoArrays/permutingTwoArrays.js
+++ permutingTwoArrays/permutingTwoArrays.js
@@ -1,1 +1,14 @@
-const permutingArrays = () => {};
+const permutingArrays = (k, a, b) => {
+  a.sort((a, b) => a - b);
+  b.sort((a, b) => b - a);
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] + b[i] < k) return "NO";
+  }
+  return "YES";
+};
+
+const k = 10;
+const a = [4, 2, 4];
+const b = [6, 7, 8];
+
+console.log(permutingArrays(k, a, b));

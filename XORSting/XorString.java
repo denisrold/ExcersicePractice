3005ff9be73c1e7f04cdfd9136e5d2d0c8087d70
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-08-13 16:48:30 -0300

    Resolved XOR in JAva

--- XORSting/XorString.java
+++ XORSting/XorString.java
@@ -3,22 +3,24 @@ package XORSting;
 import java.util.Scanner;
 
 public class XorString {
-
-    public static void main(String[] args){
+  public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         String scannerIn1 = scanner.nextLine();
         String scannerIn2 = scanner.nextLine();
-        scanner.close();
-        System.out.println(xorString(scannerIn1, scannerIn2));
+       
+        System.out.println(strings_xor(scannerIn1, scannerIn2));
     }
 
-    public static String xorString(String s1, String s2){
-        String result = "";
-        for(int i = 0; i <s1.length();i++){
-            if(s1.charAt(i) != s2.charAt(i)){result += "1";}
-            else {result += "0";}
+    public static String strings_xor(String s1, String s2){
+        String result = new String("");
+        for(int i = 0; i < s1.length(); i++){
+            if(s1.charAt(i) != s2.charAt(i)){
+                result += "1";
+            } else {
+                result += "0";
+            }
         }
-        System.out.println(result);
+   
         return result;
     }
 }

ab1ae5369dda83f7112a1b21cb38bc6d61186068
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-08-17 10:59:07 -0300

    salebymatch ts

--- salebymatch/saleByMatch.ts
+++ salebymatch/saleByMatch.ts
@@ -1,3 +1,15 @@
-const saleByMatch1 = () => {};
+const saleByMatch1 = (n:number , ar:number[]):number => {
+const Obj:Set<number> = new Set();
+let cont:number =0;
+for(const num of ar){
+    if(Obj.has(num)){
+        cont++
+        Obj.delete(num)
+    }
+    else{Obj.add(num)}
+}
 
-console.log(saleByMatch1);
+return cont;
+};
+
+console.log(saleByMatch1(9, [10, 20, 20, 10, 10, 30, 50, 10, 20]));

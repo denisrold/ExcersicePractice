fb76334cd2ab23ec14fa60700023e55b66135002
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-08-21 01:27:11 -0300

    Resolved in ts

---
+++ migratoryBirds/migatoryBirds.ts
@@ -0,0 +1,23 @@
+const migratoryBirds1 = (arr:number[]):number => {
+    let birdCount:Record<number,number> = {};
+    let commonBird:number = 0;
+    let maxCount:number = 0;
+  
+    for (let bird of arr) {
+      if (!birdCount[bird]) {
+        birdCount[bird] = 1;
+      } else {
+        birdCount[bird] += 1;
+      }
+      if (birdCount[bird] > maxCount) {
+        maxCount = birdCount[bird];
+        commonBird = bird;
+      } else if (birdCount[bird] == maxCount && commonBird > bird) {
+        commonBird = bird;
+      }
+    }
+    return commonBird;
+  };
+  
+  const birds1 = [1, 4, 4, 5, 3, 3, 3];
+  console.log(migratoryBirds1(birds1));

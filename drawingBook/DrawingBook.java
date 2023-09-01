c4af74c3d108b85a856816916e204a6a98b7b8e6
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-09-01 12:43:15 -0300

    Resolved in java

---
+++ drawingBook/DrawingBook.java
@@ -0,0 +1,18 @@
+package drawingBook;
+
+public class DrawingBook {
+    public static void main(String[] args){
+        int n = 5;
+        int p = 4;
+        System.out.println(pageCount(n,p));
+    }
+
+    public static Number pageCount(int n,int p){
+    int fromFront =(int) Math.floor(p / 2);
+  
+   
+    int fromBack = (int) (Math.floor(n / 2) - Math.floor(p / 2));
+    return Math.min(fromFront, fromBack);
+    }
+    
+}

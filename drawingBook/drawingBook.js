e750786bc36ac89601a9b8e5f079e1d90e708718
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-09-01 11:27:22 -0300

    drawingBook js

---
+++ drawingBook/drawingBook.js
@@ -0,0 +1,23 @@
+function pageCount(n, p) {
+  console.log(n, p);
+}
+
+const n = 6;
+const p = 2;
+console.log(pageCount(n, p));
+/*
+page 1 = 1, 2
+page 2 = 3, 4
+page 3 = 5, 6
+page 4 = 7, 8   
+page 5 = 8, 9
+page 6 = 10, 11 
+tengo que crear un libro con  indices segun la cantidad de pagina que me dan teniendo en cuenta la pagina 0 
+
+el largo del libro me lo describe n . que creara el indice de cantidad de paginas. 
+una pagina abarca dos numeros 
+luego debo buscar p dentro del libro y ver en que pagina esta 
+p sera el numero de hoja 
+devo devolver en que numero de pagina esta la hoja que busco. 
+
+*/

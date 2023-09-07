bc42c1ae27fbb1997a1c322988a60a9f279c4052
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-09-06 23:29:33 -0300

    3

---
+++ Week 1 2 3/twoSets/TwoSets.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class TwoSets{
+    public static void main (String[] args){}
+     public static void getTotalX(List<Integer> a, List<Integer> b) {
+   // Write your code here
+
+   }
+}
+
+    
---
+++ Week 1 2 3/twoSets/twoSets.js
@@ -0,0 +1,10 @@
+function getTotalX(a, b) {}
+
+console.log(getTotalX("a", "b"));
+
+/*    public static int getTotalX(List<Integer> a, List<Integer> b) {
+    // Write your code here
+
+    }
+
+} */

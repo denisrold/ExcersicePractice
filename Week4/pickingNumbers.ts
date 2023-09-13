b4ba4b3a27fcfa6da2f2513f0eb406fc442ac4f6
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-09-13 13:27:43 -0300

    resolved picking number in ts

--- Week4/pickingNumbers.ts
+++ Week4/pickingNumbers.ts
@@ -1,6 +1,20 @@
 const pickingNumbers1 = (a:number[]):number =>{
-console.log(a);
-return 0;
+    a.sort((x, y) => x - y);
+    console.log(a);
+    let maxCount:number = 0;
+    let prevNum= a[0];
+    let currentCount:number = 0;
+    for (let i = 0; i < a.length; i++) {
+      if ((Math.abs(a[i] - prevNum) <= 1)) {
+        currentCount++;
+      } else {
+        prevNum = a[i];
+        maxCount = Math.max(maxCount, currentCount);
+        currentCount = 1;
+      }
+    }
+    return Math.max(maxCount, currentCount);
+
 };
 
 const a1 = [4, 6, 5, 3, 3, 1];

98830a02aabc745f8ad3303ee7254f6924f62ff1
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-09-13 14:02:51 -0300

    Resolved picking numbers in java

--- Week4/PickingNumbers.java
+++ Week4/PickingNumbers.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class PickingNumbers{
@@ -8,7 +9,23 @@ public class PickingNumbers{
         a.addAll(Arrays.asList(4, 6, 5, 3, 3, 1));
         System.out.println(pickingNumbers(a));
     }
-    public static List<Integer> pickingNumbers(List<Integer> a){
-        return a;
+    public static int pickingNumbers(List<Integer> a){
+        
+        int maxCount = 0;
+        int currentCount =0;
+        int prevNumber = a.get(0);
+        Collections.sort(a);
+
+        for (int i = 0; i <a.size(); i++){
+           if(Math.abs(a.get(i) - prevNumber )<=1){
+            currentCount += 1;
+           }
+           else{
+            prevNumber= a.get(i);
+            maxCount = Math.max(maxCount,currentCount);
+            currentCount = 1;
+           }
+        }
+        return Math.max(maxCount,currentCount);
     }
 }

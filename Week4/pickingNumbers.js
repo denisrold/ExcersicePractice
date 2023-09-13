60aba432741c891d0b79fb232c72e8d55d72733c
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-09-13 13:21:57 -0300

    Resolved picking Numbers

--- Week4/pickingNumbers.js
+++ Week4/pickingNumbers.js
@@ -1,7 +1,20 @@
 const pickingNumbers = (a) => {
+  //ordeno de menor  a mayor
+  a.sort((x, y) => x - y);
+  console.log(a);
+  let maxCount = 0;
+  let prevNum = a[0];
+  let currentCount = 0;
   for (let i = 0; i < a.length; i++) {
-    console.log(a);
+    if (Math.abs(a[i] - prevNum <= 1)) {
+      currentCount++;
+    } else {
+      prevNum = a[i];
+      maxCount = Math.max(maxCount, currentCount);
+      currentCount = 1;
+    }
   }
+  return Math.max(maxCount, currentCount);
 };
 
 const a = [4, 6, 5, 3, 3, 1];

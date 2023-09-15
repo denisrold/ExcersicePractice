e16b26d5b81094e50d97c9ab755e17ce6909355d
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-09-15 15:46:04 -0300

    leftRotation in java

---
+++ Week4/leftRotation/LeftRotation.java
@@ -0,0 +1,21 @@
+package leftRotation;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class LeftRotation {
+        public static List<Integer> rotateLeft(int d, List<Integer> arr) {
+    // Rota los numeros hacia izquierda o derecha. 
+    Collections.rotate(arr, -d);
+    return arr;
+    }
+    public static void main (String[]  args){
+        List<Integer> arr = new ArrayList<>();
+        arr.addAll(Arrays.asList(1, 2, 3, 4, 5));
+        int d = 54;
+        System.out.println(rotateLeft(d, arr));
+    }
+}

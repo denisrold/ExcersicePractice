b18758b957c23cb07af2915cc9b08e18e3a5ce92
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-09-20 16:23:16 -0300

    Kangaroo Resolved in ts

--- Week4/kangaroo/kangaroo.ts
+++ Week4/kangaroo/kangaroo.ts
@@ -1,2 +1,12 @@
-function kangaroo1() {}
-console.log(kangaroo1());
+function kangaroo1(xa:number, va:number, xb:number, vb:number):String {
+    if(va===vb && xa !==xb){return 'NO'};
+    if((xb - xa) % (va - vb) === 0 && (xb - xa) / (va - vb) >= 0){return 'YES'}
+   
+    return 'NO'
+}
+let xa = 0;
+let va = 3;
+let xb = 4;
+let vb = 2;
+
+console.log(kangaroo1(xa, va, xb, vb));

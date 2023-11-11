999aeb3baf7e350d33339a5bcee970ae955861fa
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-11-11 19:05:36 -0300

    tps ok

---
+++ Week4/caesarsCipher/caesarsCipher.ts
@@ -0,0 +1,22 @@
+
+function caesarsCipher3 (s2:string, k2:number):string {
+    let newString = "";
+    for (let i = 0; i < s2.length; i++) {
+      if (s2[i].match(/[a-zA-Z]/)) {
+        const codigoAscii = s2[i].charCodeAt(0);
+        let offset = codigoAscii < 91 ? 65 : 97;
+        const nuevoCodigoAscii = ((codigoAscii - offset + k2) % 26) + offset;
+        newString += String.fromCharCode(nuevoCodigoAscii);
+      } else {
+        newString += s2[i];
+      }
+    }
+
+    return newString;
+  };
+
+let s2 = "middle-Outz";
+let k2 = 2;
+
+caesarsCipher3(s2, k2);
+//okffng-Qwvb

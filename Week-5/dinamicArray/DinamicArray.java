c72798cc17fd1b030afd6139425ab2acbc457887
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-12-18 19:36:28 -0300

    Starting dinamyc array

---
+++ Week-5/dinamicArray/DinamicArray.java
@@ -0,0 +1,12 @@
+package dinamicArray;
+
+public class DinamicArray {
+    public static void main(String[] args){
+    System.out.println(dinamicArray());
+    }
+
+    public static int dinamicArray(){
+        return 1;
+    };
+    
+} 

71f92053a4f9874492436354805a05bace09bede
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2024-01-28 16:39:12 -0300

    Java resolved

--- Week-5/MaxMin/Maxmin.java
+++ Week-5/MaxMin/Maxmin.java
@@ -1,3 +1,28 @@
+package MaxMin;
+
+
+import java.util.*;
+
+
 public class Maxmin {
-    
+    public static void main(String args[]){
+       int k = 3;
+        List<Integer> arr = new ArrayList<>();
+       arr.addAll(Arrays.asList(7, 3, 10, 100, 300, 200, 1000, 20, 30));
+
+       System.out.println(maxmin(3, arr)); 
+    }
+    public static Integer maxmin(int k, List<Integer> arr ){
+            Collections.sort(arr);
+            Integer min = Integer.MAX_VALUE ;
+
+            for(int i = 0 ; i <= (k - 1); i++ ){
+            Long injusticia  =  arr.get(i + k -1).longValue() - arr.get(i).longValue();
+            
+            if(injusticia.intValue() < min.intValue()){
+                min = injusticia.intValue();
+            }    
+        }
+        return min;
+    }
 }
